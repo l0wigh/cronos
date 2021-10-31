331de966221b9456a41f76aa5bb286c973a3a172
L0Wigh <mathiotthomas@gmail.com>
2021-10-31 14:26:29 +0100

    Adding line calc

--- cronos.c
+++ cronos.c
@@ -3,6 +3,18 @@
 #include <string.h>
 #include <stdio.h>
 
+int calculate_line(FILE *calcFile) {
+	char chr = getc(calcFile);
+	int countLine = 0;
+	while(chr != EOF) {
+		if(chr == "n") {
+			countLine++;
+		}
+		chr = getc(calcFile);
+	}
+	return countLine;
+}
+
 void rockthehash(char *string, char *hash) {
 	int i;
 	char unsigned md5[MD5_DIGEST_LENGTH] = {0};
@@ -29,7 +41,10 @@ int main(int argc, char *argv[]) {
 		rockyoufile = fopen(argv[1], "r");
 		char line[255];
 		printf("Cronos is cracking the hash...\n");
+		int totalLine = calculate_line(rockyoufile);
+		int currentLine = 1;
 		while (fgets(line, sizeof(line), rockyoufile)) {
+			printf("\r%d / %d\n", currentLine, totalLine);
 			line[strcspn(line, "\n")] = 0;
 			rockthehash(line, md5_hash);
 			if (memcmp(argv[2], md5_hash, 16) == 0) {
